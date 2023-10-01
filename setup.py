3f799f23251f72cbeff4e9c167cfb3b1f7eb0bd5
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2023-10-01 18:03:57 +0900

    Modify to use Python 3.6 syntaxes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 
 MODULE_NAME = "grsched"
-REPOSITORY_URL = "https://github.com/thombashi/{:s}".format(MODULE_NAME)
+REPOSITORY_URL = f"https://github.com/thombashi/{MODULE_NAME:s}"
 REQUIREMENT_DIR = "requirements"
 ENCODING = "utf8"
 
