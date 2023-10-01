0eb3cc13fd3d631b786ae01075e316129dc9db03
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2023-10-01 22:08:19 +0900

    Add support for Python 3.10/3.11

--- setup.py
+++ setup.py
@@ -62,6 +62,8 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3 :: Only",
         "Topic :: Office/Business :: Groupware",
         "Topic :: Office/Business :: Scheduling",
